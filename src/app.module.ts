046d0ed2edc75ba15b5934b34ad55143c0408e94
LeVanLinh2307 <113876591+LeVanLinh2307@users.noreply.github.com>
2022-09-28 01:24:09 +0700

    auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm/dist';
 import { DatabaseConnectionService } from './database-connection.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
       useClass: DatabaseConnectionService,
     }),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
