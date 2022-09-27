1a1488a2297c0a3db022e2bcc4d539a9d8c02780
LeVanLinh2307 <113876591+LeVanLinh2307@users.noreply.github.com>
2022-09-28 00:31:28 +0700

    connect db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm/dist';
+import { DatabaseConnectionService } from './database-connection.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useClass: DatabaseConnectionService,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
